c84bcf553b34da711bdc59494ff4d2c3b9dd0a56
Lucas de Miranda <lucaslima939@gmail.com>
2025-06-13 22:08:28 -0300

    feat: implement get it

---
+++ lib/injection_container.dart
@@ -0,0 +1,41 @@
+import 'package:cos_challenge/data/adapters/local_storage_adapter.dart';
+import 'package:cos_challenge/data/data_sources/auth_data_source.dart';
+import 'package:cos_challenge/domain/repositories/auth_repository.dart';
+import 'package:cos_challenge/domain/use_cases/is_user_authenticated_use_case.dart';
+import 'package:cos_challenge/presentation/cubits/splash/splash_cubit.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> initContainer() async {
+  // Shared Preferences
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  // Adapters
+  getIt.registerSingleton<LocalStorageAdapter>(
+    SpLocalStorageAdapterImpl(sharedPreferences: sharedPreferences),
+  );
+
+  // Data Sources
+  getIt.registerSingleton<AuthDataSource>(
+    AuthDataSourceImpl(localStorage: getIt<LocalStorageAdapter>()),
+  );
+
+  // Repositories
+  getIt.registerSingleton<AuthRepository>(
+    AuthRepositoryImpl(dataSource: getIt<AuthDataSource>()),
+  );
+
+  // Use Cases
+  getIt.registerSingleton<IsUserAuthenticatedUseCase>(
+    IsUserAuthenticatedUseCase(repository: getIt<AuthRepository>()),
+  );
+
+  // Cubits
+  getIt.registerSingleton<SplashCubit>(
+    SplashCubit(
+      isUserAuthenticatedUseCase: getIt<IsUserAuthenticatedUseCase>(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
-import 'package:cos_challenge/data/adapters/local_storage_adapter.dart';
-import 'package:cos_challenge/data/data_sources/auth_data_source.dart';
-import 'package:cos_challenge/domain/repositories/auth_repository.dart';
-import 'package:cos_challenge/my_app.dart';
+import 'package:cos_challenge/core/theme/app_theme.dart';
+import 'package:cos_challenge/injection_container.dart';
+import 'package:cos_challenge/presentation/cubits/splash/splash_cubit.dart';
+import 'package:cos_challenge/presentation/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp(authRepository: await getAuthRepository()));
+  await initContainer();
+  runApp(const MyApp());
 }
 
-Future getAuthRepository() async {
-  final localStorage = SpLocalStorageAdapterImpl(
-    sharedPreferences: await SharedPreferences.getInstance(),
-  );
-  final dataSource = AuthDataSourceImpl(localStorage: localStorage);
-  return AuthRepositoryImpl(dataSource: dataSource);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CarOnSale',
+      theme: AppTheme.light,
+      home: MultiBlocProvider(
+        providers: [BlocProvider(create: (context) => getIt<SplashCubit>())],
+        child: const SplashScreen(),
+      ),
+    );
+  }
 }
